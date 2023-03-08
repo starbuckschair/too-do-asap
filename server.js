587fa9bea191524630ff3e13b5748e8167ee2d54
starbuckschair <93638998+starbuckschair@users.noreply.github.com>
2023-03-08 14:45:43 +0900

    git test

---
+++ server.js
@@ -0,0 +1,49 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const MongoClient = require('mongodb').MongoClient;
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extended:true}));
+
+
+MongoClient.connect("mongodb+srv://admin:qwer1234@cluster0.mdran.mongodb.net/?retryWrites=true&w=majority", function(에러, client){
+  if (에러) return console.log(에러);
+  //서버띄우는 코드 여기로 옮기기
+
+  db = client.db('todoapp');
+
+  app.listen('8080', function(){
+    console.log('listening on 8080')
+  });
+})
+
+
+// app.listen(8080, function () {
+//   console.log('listening on 8080')
+// }); 
+
+app.use(express.json());
+var cors = require('cors');
+app.use(cors());
+
+app.use(express.static(path.join(__dirname,'just-do-it/build')));
+
+app.post('/add', function(요청,응답){
+  응답.send('전송완료');
+  console.log(요청.body.data);
+  console.log(요청.body.title);
+  db.collection('post').insertOne({제목: 요청.body.title, 날짜: 요청.body.date}, function(){console.log('저장완료')})
+})
+
+app.get('/', function(요청, 응답){
+  응답.sendFile(path.join(__dirname,"just-do-it/build/index.html" ))
+})
+
+
+app.get('/product', function(요청, 응답){
+  응답.json({name : 'black shoes'})
+})
+
+app.get('*', function(요청, 응답){
+  응답.sendFile(path.join(__dirname,"just-do-it/build/index.html" ))
+})
